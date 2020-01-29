31408215e64ed995b3205580953da169db24f9ac
denis <025896321Denis>
2020-01-29 12:37:47 +0200

    g

--- app/src/main/java/com/battleking/a2048/constants/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.battleking.a2048.constants;
-
-public class Constants {
-    public static final String MYLOG_TEG = "my log";
-    public static final String URL = "https://battle-king.fun";
-    public static final String DEPLINK = "pls://run";
-
-}
