1a5d86d1498326b532184af19f1eeb98bcc41460
denis <025896321Denis>
2020-01-31 16:53:51 +0200

    g

--- app/src/main/java/com/piramideofra/aprw/App.java
+++ app/src/main/java/com/piramideofra/aprw/App.java
@@ -2,6 +2,7 @@ package com.piramideofra.aprw;
 
 import android.app.Application;
 
+import com.onesignal.OneSignal;
 import com.piramideofra.aprw.manedger.game.GamePreferences;
 
 public class App extends Application {
@@ -11,10 +12,10 @@ public class App extends Application {
         super.onCreate();
         // OneSignal Initialization
 
-//        OneSignal.startInit(this)
-//                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
-//                .unsubscribeWhenNotificationsAreDisabled(true)
-//                .init();
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
 
         GamePreferences.initialize(this);
 
